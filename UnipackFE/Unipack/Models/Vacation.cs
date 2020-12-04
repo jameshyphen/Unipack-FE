fa5febad0b9d4bbc65354767243324e2b0b1a125
lunadv <luna.devuyst@student.hogent.be>
2020-12-04 17:11:07 +0100

    add models

---
+++ UnipackFE/Unipack/Models/Vacation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unipack.Models
+{
+    public class Vacation
+    {
+        public int VacationId { get; set; }
+        public string Name { get; set; }
+        public ICollection<VacationLocation> Locations { get; set; }
+        public ICollection<VacationList> VacationLists { get; set; }
+        public DateTime DateDeparture { get; set; }
+        public DateTime DateReturn { get; set; }
+        public DateTime AddedOn { get; set; }
+    }
+}
---
+++ UnipackFE/Unipack/Models/VacationListItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Unipack.Models
+{
+    public class VacationListItem
+    {
+        public int VacationListItemId { get; set; }
+        
+        public Item Item { get; set; }
+        public VacationList VacationList { get; set; }
+        public int Quantity { get; set; }
+        public DateTime AddedOn { get; set; }
+    }
+}
