e31cf006a97af4b863535e531892d8120271eeba
Tijl Zwartjes <tijl.zwartjes.y1524@student.hogent.be>
2021-01-01 18:57:00 +0100

    added Binding+ add functionality
    created viewmodel for category with mock data.
    added add category content dialog with functionality (TODO api call)
    added edit category content dialog (TODO functionality)

--- UnipackFE/Unipack/Views/CategoryPage.xaml.cs
+++ UnipackFE/Unipack/Views/CategoryPage.xaml.cs
@@ -16,46 +16,94 @@ using Windows.UI.Xaml.Navigation;
 using Unipack.Models;
 using Unipack.ViewModels;
 using System.Net.Http;
+using Unipack.Views.Dialogs;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace Unipack.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class CategoryPage : Page
     {
-        private AuthenticationViewModel authenticationVM;
-        public ObservableCollection<Category> categories { get; set; }
+        private AuthenticationViewModel _authenticationVM;
+        private CategoryViewModel _categoryVM;
+        public RelayCommand<string> DeleteCommand;
 
         public CategoryPage()
         {
             this.InitializeComponent();
-
-            categories = new ObservableCollection<Category>();
-
-            AddCategory(new Category() {AddedOn = DateTime.Now, Name = "Tech", NumberOfItems = 12});
-            AddCategory(new Category() {AddedOn = DateTime.Now, Name = "Books", NumberOfItems = 31 });
-            AddCategory(new Category() {AddedOn = DateTime.Now, Name = "Food", NumberOfItems = 15 });
+            _categoryVM = new CategoryViewModel();
+            InitializeCategories();
+            CategoryGrid.DataContext = _categoryVM.categories;
+            DeleteCommand = new RelayCommand<string>((param) => DeleteCategory(param));
         }
 
-        public void AddCategory(Category cat)
+        public CategoryPage(AuthenticationViewModel auth)
         {
-            categories.Add(cat);
+            this.InitializeComponent();
+            _categoryVM = new CategoryViewModel();
+            InitializeCategories();
+            CategoryGrid.DataContext = _categoryVM.categories;
+            _authenticationVM = auth;
+        }
+
+        private void InitializeCategories()
+        {
+            //TODO hier api call doen voor alle categoriën te adden & demo data verwijderen
+            _categoryVM.AddCategory(new Category() { AddedOn = DateTime.Now, Name = "Tech", NumberOfItems = 12 });
+            _categoryVM.AddCategory(new Category() { AddedOn = DateTime.Now, Name = "Books", NumberOfItems = 31 });
+            _categoryVM.AddCategory(new Category() { AddedOn = DateTime.Now, Name = "Food", NumberOfItems = 15 });
+        }
+
+        private async void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            CategoryAddContentDialog addContentDialog = new CategoryAddContentDialog(_authenticationVM, _categoryVM);
+
+            await addContentDialog.ShowAsync();
+            if (!addContentDialog.Success)
+                return;
+
+        }
+        public void DeleteCategory(string cat)
+        {
+            _categoryVM.DeleteCategory(GetCategoryByName(cat));
+            //TODO hier pai call doen voor categorie te verwijderen
+        }
+        private Category GetCategoryByName(string name)
+        {
+            try
+            {
+                return _categoryVM.categories.First(x => x.Name.Equals(name));
+            }
+            catch
+            {
+                return null;
+            }
         }
 
         public void Clear()
         {
-            categories.Clear();
+            _categoryVM.Clear();
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
 
-            authenticationVM = (AuthenticationViewModel)e.Parameter;
+            _authenticationVM = (AuthenticationViewModel)e.Parameter;
+        }
+
+        public void CategoryGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void CategoryGrid_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            Category category = (Category) e.ClickedItem;
+            //TODO doe hier iets mee
         }
     }
 }
-
