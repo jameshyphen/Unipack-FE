8452d008a358aa712e1cb008f7440d7bc2d5462c
Tijl Zwartjes <tijl.zwartjes.y1524@student.hogent.be>
2021-01-16 14:33:04 +0100

    merge
    Co-Authored-By: James Hyphen <james.hyphen@gmail.com>
    Co-Authored-By: lunadv <36530736+lunadv@users.noreply.github.com>

--- UnipackFE/Unipack/Views/VacationPage.xaml.cs
+++ UnipackFE/Unipack/Views/VacationPage.xaml.cs
@@ -36,16 +36,12 @@ namespace Unipack.Views
             authVM = authvm;
             Success = false;
         }
-        public VacationPage(AuthenticationViewModel authvm) : this()
-        {
-            authVM = authvm;
-        }
 
-        public async void getData()
+        public async void GetData()
         {
 
             HttpClient client = new HttpClient();
-            var json = await client.GetStringAsync(new Uri("http://195.201.101.209/unipack/api/vacation"));
+            var json = await client.GetStringAsync(new Uri("http://hyphen-solutions.be/unipack/api/vacation"));
             var vacationList = JsonConvert.DeserializeObject<ICollection<Vacation>>(json);
             vacationsListView.ItemsSource = vacationList;
 
